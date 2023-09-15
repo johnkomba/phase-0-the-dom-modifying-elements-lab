bb114b38669946dabf9e3730b154a68dea5d27ca
Jkomba <johnkombah88@gmail.com>
2023-09-15 21:09:05 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// index.js
+
+// Remove the main element with id 'main'
+document.getElementById('main').remove();
+
+// Create a new header element with id 'newHeader'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new header
+newHeader.textContent = 'John Komba is the champion'; // Replace YOUR-NAME with your desired name
+
+// Append the new header to the body of the document
+document.body.appendChild(newHeader);
